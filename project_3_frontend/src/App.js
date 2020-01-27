87b384621785a5331d430622333012b67e501b17
David Loesch <davidloesch1@gmail.com>
2020-01-27 11:22:32 -0800

    adding style to front end app component

--- project_3_frontend/src/App.js
+++ project_3_frontend/src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container app-body">
+      <div className="bg"></div>
+      <div className="text-container">
+        <h1 className="page-header">Kollage</h1>
+        <h5 className="page-sub-header">Open Photo Sharing</h5>
+        <p className="lead">Share your photos with the world</p>
+      </div>
     </div>
   );
 }
